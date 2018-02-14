477cf7b57ea43905cbbc00e7e61d1fe02190bad8
Xavier Burrow <jxburrow@gmail.com>
2018-02-14 13:23:32 +1100

    animated sequences ..

--- nav.php
+++ nav.php
@@ -1,7 +1,23 @@
 <div class='nav'>
   <div class='nav__inner'>
-    <div class='nav__inner__third'>left</div>
-    <div class='nav__inner__third'>centre</div>
-    <div class='nav__inner__third'>right?</div>
+    <div class='nav__inner__third'>
+      <div class='nav-item'>
+        <div class='nav-item__inner'>Home</div>
+      </div>
+    </div>
+    <div class='nav__inner__third'>
+      <div class='nav-logo'><( 0 )></div>
+    </div>
+    <div class='nav__inner__third'>
+      <div class='nav-item'>
+        <div class='nav-item__inner'>Work</div>
+      </div>
+      <div class='nav-item'>
+        <div class='nav-item__inner'>Info</div>
+      </div>
+      <div class='nav-item'>
+        <div class='nav-item__inner'>Instagram</div>
+      </div>
+    </div>
   </div>
 </div>
--- section-projects-item.php
+++ section-projects-item.php
@@ -1,21 +1,22 @@
 <?php
   $title = get_the_title();
   $type = get_field('menu_type');
-  $img = get_field('menu_image');
-  $hover = get_field('hover_image');
-  //$desc = get_field('description');
-  //$video = get_field('video');
-  //$gallery = get_field('gallery');
-  //$links = get_field('links');
+  $image1 = get_field('menu_image');
+  $image2 = get_field('hover_image');
+  $sequence = get_field('image_sequence');
 ?>
 
 <div class='item'>
-  <div class='item__inner <?php echo $type; ?>'>
-    <?php if ($type == 'image_hover'): ?>
-      <img class='hover' src='<?php echo $img['sizes']['medium']; ?>' />
-      <img class='hover' src='<?php echo $hover['sizes']['medium']; ?>' />
-    <?php else: ?>
-      <img src='<?php echo $img['sizes']['medium']; ?>' />
-    <?php endif; ?>
+  <div class='item__inner <?php echo $type; ?>' data-id='<?php echo get_the_ID(); ?>'>
+    <?php if ($type == 'image_static'): ?>
+      <img src='<?php echo $image1['sizes']['large']; ?>' />
+    <?php elseif ($type == 'image_hover'): ?>
+      <img class='hover' src='<?php echo $image1['sizes']['large']; ?>' />
+      <img class='hover' src='<?php echo $image2['sizes']['large']; ?>' />
+    <?php elseif ($type == 'image_sequence'):
+      $i = 0;
+      foreach ($sequence as $img): ?>
+      <img class='sequence <?php if ($i++ == 0){ echo 'active'; } ?>' src='<?php echo $img['sizes']['large']; ?>' />
+    <?php endforeach; endif; ?>
   </div>
 </div>
